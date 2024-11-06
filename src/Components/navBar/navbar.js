1fa2b6bf8585a778e91b9eab9b41f6a8af934428
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-11-06 17:01:51 -0500

    format navbar

--- src/Components/navBar/navbar.js
+++ src/Components/navBar/navbar.js
@@ -5,51 +5,54 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 function Navigate(props) {
   // const user = props.user.username;
-  const user = "LongNameSuckerPerson";
-  return (
-    <div className="navstyle">
-      <Container>
-        <Navbar
-          collapseOnSelect
-          bg="light"
-          variant="light"
-          expand="md"
-          fixed="top"
-        >
-          <Container className="navbar-container">
-            <Navbar.Brand>
-              <Link class="brand" to="/">
-                HornTrax
-              </Link>
-            </Navbar.Brand>
-            {/* <p className="welcomeMessage">Welcome back, {user}!</p> */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto links">
-                {props.user ? (
-                  <Link
-                    className="logoutLink"
-                    to="/horntrax_v2.0"
-                    onClick={props.onLoggedOut}
-                  >
-                    Log Out
-                  </Link>
-                ) : (
-                  <Link
-                    className="loginLink"
-                    to="/login"
-                    onClick={props.onLoggedOut}
-                  >
-                    Log In
-                  </Link>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </Container>
-    </div>
-  );
+let user = "long ass crazy name";
+return (
+  <div className="navstyle">
+    <Container>
+      <Navbar
+        collapseOnSelect
+        bg="light"
+        variant="light"
+        expand="md"
+        fixed="top"
+      >
+        <Container className="navbar-container">
+          <Navbar.Brand>
+            <Link class="brand" to="/">
+              HornTrax
+            </Link>
+          </Navbar.Brand>
+          <div className="welcomeMessage">
+            Welcome back, {user.substring(0, 13)}!
+          </div>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto links">
+              {props.user ? (
+                <Link
+                  className="logoutLink"
+                  to="/horntrax_v2.0"
+                  onClick={props.onLoggedOut}
+                >
+                  Log Out
+                </Link>
+              ) : (
+                <Link
+                  className="loginLink"
+                  to="/login"
+                  onClick={props.onLoggedOut}
+                >
+                  Log In
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  </div>
+);
 }
 
 export default Navigate;
